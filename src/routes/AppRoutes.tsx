087c2c2286cb047ca14711f47fdaa315633688b2
vclong2003 <vclong2003@gmail.com>
2024-04-01 15:22:45 +0700

    Update global.css and AppRoutes.tsx

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,3 +1,23 @@
+import Button from "@components/Button/Button";
+import Input from "@components/formComponents/Input/Input";
+import { Select } from "antd";
+import { default as MySelect } from "@components/formComponents/Select/Select";
+
 export default function AppRoutes() {
-  return <></>;
+  return (
+    <div>
+      <Input type="text" />
+      <Select>
+        <Select.Option value="1">Option 1</Select.Option>
+        <Select.Option value="2">Option 2</Select.Option>
+        <Select.Option value="3">Option 3</Select.Option>
+      </Select>
+      <MySelect>
+        <Select.Option value="1">Option 1</Select.Option>
+        <Select.Option value="2">Option 2</Select.Option>
+        <Select.Option value="3">Option 3</Select.Option>
+      </MySelect>
+      <Button>Click me</Button>
+    </div>
+  );
 }
