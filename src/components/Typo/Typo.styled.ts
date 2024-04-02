5d6d52f3751928c97e0d795a0d402960bcbda376
vclong2003 <vclong2003@gmail.com>
2024-04-02 11:36:38 +0700

    Add Typo component and its styled file

---
+++ src/components/Typo/Typo.styled.ts
@@ -0,0 +1,46 @@
+import styled, { css } from "styled-components";
+import { Weight } from "./Typo";
+
+interface ITypoProps {
+  $weight?: Weight;
+}
+
+const BaseStyle = css<ITypoProps>`
+  font-family: ${({ $weight }) =>
+    $weight === "medium" ? "var(--ff-medium)" : "var(--ff-regular)"};
+`;
+
+export const H1 = styled.h1<ITypoProps>`
+  ${BaseStyle}
+  font-size: var(--fs-h1);
+`;
+
+export const H2 = styled.h2<ITypoProps>`
+  ${BaseStyle}
+  font-size: var(--fs-h2);
+`;
+
+export const H3 = styled.h3<ITypoProps>`
+  ${BaseStyle}
+  font-size: var(--fs-h3);
+`;
+
+export const H4 = styled.h4<ITypoProps>`
+  ${BaseStyle}
+  font-size: var(--fs-h4);
+`;
+
+export const H5 = styled.h5<ITypoProps>`
+  ${BaseStyle}
+  font-size: var(--fs-h5);
+`;
+
+export const Body1 = styled.p<ITypoProps>`
+  ${BaseStyle}
+  font-size: var(--fs-body1);
+`;
+
+export const Body2 = styled.p<ITypoProps>`
+  ${BaseStyle}
+  font-size: var(--fs-body2);
+`;
---
+++ src/components/Typo/Typo.tsx
@@ -0,0 +1,31 @@
+import * as S from "./Typo.styled";
+
+export type Weight = "regular" | "medium";
+type Variant = "h1" | "h2" | "h3" | "h4" | "h5" | "body1" | "body2";
+
+interface ITypoProps extends JSX.IntrinsicAttributes {
+  children: React.ReactNode;
+  weight?: Weight;
+  variant: Variant;
+}
+
+export default function Typo({ variant, children, weight }: ITypoProps) {
+  switch (variant) {
+    case "h1":
+      return <S.H1 weight={weight}>{children}</S.H1>;
+    case "h2":
+      return <S.H2 weight={weight}>{children}</S.H2>;
+    case "h3":
+      return <S.H3 weight={weight}>{children}</S.H3>;
+    case "h4":
+      return <S.H4 weight={weight}>{children}</S.H4>;
+    case "h5":
+      return <S.H5 weight={weight}>{children}</S.H5>;
+    case "body1":
+      return <S.Body1 weight={weight}>{children}</S.Body1>;
+    case "body2":
+      return <S.Body2 weight={weight}>{children}</S.Body2>;
+    default:
+      return null;
+  }
+}
