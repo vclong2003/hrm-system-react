7b30976746bf86fb3551200d34803f1d9aa7c70e
vclong2003 <vclong2003@gmail.com>
2024-04-19 09:58:57 +0700

    Add new interfaces for benefits and grades

---
+++ src/interfaces/benefit.ts
@@ -0,0 +1,16 @@
+export interface IBenefit {
+  id: number;
+  name: string;
+  code: string;
+  type: number;
+  value: string;
+  company_id: number;
+  created_at: string;
+  updated_at: string;
+}
+
+// Get Benefit List -----------------------------------
+export interface IGetBenefitListPayload {
+  search: string;
+  grade_id: number;
+}
---
+++ src/interfaces/grade.ts
@@ -0,0 +1,16 @@
+import { IBenefit } from "./benefit";
+
+export interface IGrade {
+  id: number;
+  name: string;
+  prefix: string;
+  company_id: number;
+  created_at: string;
+  updated_at: string;
+  benefits: IBenefit[];
+}
+
+// Get Grade List -------------------------------------
+export interface IGetGradeListPayload {
+  search: string;
+}
