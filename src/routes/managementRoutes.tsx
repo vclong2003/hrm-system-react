5ff2bd10ce26845148f08885da904aadeac8f3c2
vclong2003 <vclong2003@gmail.com>
2024-04-21 17:37:05 +0700

    ok

--- src/routes/managementRoutes.tsx
+++ src/routes/managementRoutes.tsx
@@ -1,8 +1,9 @@
 import { IRoute } from "@interfaces/common";
+import EmployeeManagement from "@pages/EmployeeManagement/EmployeeManagement";
 
 export const managementRoutes: IRoute[] = [
   {
     path: "/",
-    component: <div>Management</div>,
+    component: <EmployeeManagement />,
   },
 ];
