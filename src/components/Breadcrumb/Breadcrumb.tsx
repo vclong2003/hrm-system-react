4d400301344a582fae0a4e719175fda8aa5043a7
vclong2003 <vclong2003@gmail.com>
2024-04-22 10:50:17 +0700

    Breadcrumb

--- src/components/Breadcrumb/Breadcrumb.tsx
+++ src/components/Breadcrumb/Breadcrumb.tsx
@@ -3,15 +3,22 @@ import RightArrow from "@assets/icons/right_arrow.png";
 
 import { Breadcrumb as AntBreadcrumb } from "antd";
 import { BreadcrumbItemType } from "antd/es/breadcrumb/Breadcrumb";
+import { Link } from "react-router-dom";
 
 interface IBreadcrumbProps {
   items: BreadcrumbItemType[];
 }
 
 export default function Breadcrumb({ items }: IBreadcrumbProps) {
+  const itemRender = (route: BreadcrumbItemType) => {
+    console.log(route);
+    if (route.path) return <Link to={`/${route.path}`}>{route.title}</Link>;
+    return <span>{route.title}</span>;
+  };
   return (
     <AntBreadcrumb
       separator={<S.SeperatorIcon src={RightArrow} />}
+      itemRender={itemRender}
       items={items}
     />
   );
