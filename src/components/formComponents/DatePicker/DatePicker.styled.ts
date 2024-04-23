d29ffa4c7833c8fb4be98276cd125459cc1110c5
vclong2003 <vclong2003@gmail.com>
2024-04-23 08:59:17 +0700

    Refactor DatePicker component styles

--- src/components/formComponents/DatePicker/DatePicker.styled.ts
+++ src/components/formComponents/DatePicker/DatePicker.styled.ts
@@ -5,4 +5,7 @@ export const DatePicker = styled(BaseDatePicker)`
   height: 40px;
   border: none;
   background-color: var(--c-white-1);
+  input {
+    font-size: var(--fs-body1) !important;
+  }
 `;
