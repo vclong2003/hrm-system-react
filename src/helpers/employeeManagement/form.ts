a61405681f55bd98df39321b2a621059cd24fa87
vclong2003 <vclong2003@gmail.com>
2024-04-24 11:59:25 +0700

    Add employee management form helper functions and update tsconfig paths

---
+++ src/helpers/employeeManagement/form.ts
@@ -0,0 +1,22 @@
+import { ICreateEmployeePayload, IEmployee } from "@interfaces/employee";
+
+export const sanitizeEmployeeData = (
+  employee: IEmployee
+): ICreateEmployeePayload => {
+  const sanitizedData: Partial<ICreateEmployeePayload> = {};
+
+  Object.keys(employee).forEach((key) => {
+    if (key in employee) {
+      sanitizedData[key as keyof ICreateEmployeePayload] =
+        employee[key as keyof IEmployee];
+    }
+  });
+
+  return sanitizedData as ICreateEmployeePayload;
+};
+
+const checkErrors = (errors: { [key: string]: unknown }, fields: string[]) => {
+  return fields.some((field) => Boolean(errors[field]));
+};
+
+export default { checkErrors, sanitizeEmployeeData };
