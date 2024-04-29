25c4a4762f83679b20ebc75f27722c7b204bbf65
vclong2003 <vclong2003@gmail.com>
2024-04-29 16:47:09 +0700

    Refactor DatePicker component to use dayjs instead of moment

--- src/components/formComponents/DatePicker/DatePicker.tsx
+++ src/components/formComponents/DatePicker/DatePicker.tsx
@@ -1,7 +1,7 @@
 import { DatePickerProps } from "antd";
 import * as S from "./DatePicker.styled";
 import { useFormikContext } from "formik";
-import moment from "moment";
+import dayjs from "dayjs";
 
 interface IDatePickerProps extends DatePickerProps {
   name: string;
@@ -10,11 +10,19 @@ export default function DatePicker({ name }: IDatePickerProps) {
   const { setFieldValue, values } = useFormikContext<{
     [key: string]: string;
   }>();
+
   const onChange = (_: unknown, dateString: string | string[]) => {
+    console.log(dateString);
     setFieldValue(name, dateString);
   };
 
-  const dateValue = values[name] ? moment(values[name]) : null;
+  console.log(values[name]); // result 1991-05-04
 
-  return <S.DatePicker onChange={onChange} value={dateValue} />;
+  return (
+    <S.DatePicker
+      onChange={onChange}
+      format="YYYY-MM-DD"
+      value={values[name] ? dayjs(values[name]) : null}
+    />
+  );
 }
